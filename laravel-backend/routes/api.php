91516eac2df227e7ee323bf57339aae9de6580b7
Nish323 <doragon.nini@icloud.com>
2025-03-02 15:26:59 +0900

    マイグレーションとシーディング

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API SUCCESS (Laravel 11)']);
 });
+
+Route::get('/posts', [PostController::class, 'index']);
